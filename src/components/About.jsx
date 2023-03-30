f0c9ec49c0a0ecf39d532a50fbb7aaada74c7e6f
Fabrice <codinglone@gmail.com>
2023-03-30 13:25:45 +0200

    Added changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div>
       <img
-        src="https://images.unsplash.com/photo-1534665482403-a909d0d97c67?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1tZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
+        src={People}
         style={{ width: "600px" }}
         alt=""
       />
